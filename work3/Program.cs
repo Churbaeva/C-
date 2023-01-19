eb654b04625dbe63ebe86ad7e87874467c006037
churbaeva <fedosova1997@mail.ru>
2023-01-19 17:23:04 +0500

    33

---
+++ work3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("insert the number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}

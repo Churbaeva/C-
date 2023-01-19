2591b0ad110f509470bf0a2aec0fa2fee0889f21
churbaeva <fedosova1997@mail.ru>
2023-01-19 17:20:28 +0500

    11

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("insert the number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 2)
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}
---
+++ work1/Program.cs
@@ -0,0 +1,5 @@
+﻿
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int maximum =Math.Max(number1, number2);
+System.Console.WriteLine(maximum);

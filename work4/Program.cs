6c709bc887884d343f26b58a62fa519f554a918e
churbaeva <fedosova1997@mail.ru>
2023-01-19 17:26:58 +0500

    44

---
+++ work4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("insert the number");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int currentNumber = 2; currentNumber<=number; currentNumber +=2)
+{
+    System.Console.WriteLine(currentNumber + "");
+}

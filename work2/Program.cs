66e2b4f8e52124dcdbbbbffead68d01095d524bd
churbaeva <fedosova1997@mail.ru>
2023-01-19 17:22:34 +0500

    2

---
+++ work2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maximum = (Math.Max(number1, Math.Max(number2, number3)));
+System.Console.WriteLine(maximum);
